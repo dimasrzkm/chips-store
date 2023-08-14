c4de2a07c626234af1eeb1bff587e54a3a2990f6
Dimas Rizki Mahendra <dimas1.mahendra@gmail.com>
2023-08-14 13:54:31 +0700

    Update Web Route for Permissions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\LoginController;
 use App\Livewire\Pages\Admin;
+use App\Livewire\Permissions\CreatePermissions;
+use App\Livewire\Permissions\EditPermissions;
+use App\Livewire\Permissions\ShowPermissions;
 use App\Livewire\Profile\ProfileInformation;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +18,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', Admin::class)->name('dashboard');
     Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
     Route::get('profile/{user}', ProfileInformation::class)->name('profile');
+
+    Route::prefix('permissions')->group(function () {
+        Route::get('', ShowPermissions::class)->name('permissions.index');
+        Route::get('create', CreatePermissions::class)->name('permissions.create');
+        Route::get('{permission:name}/edit', EditPermissions::class)->name('permissions.edit');
+    });
 });
