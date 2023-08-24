f4e46fafb3ddddb7c1f91f73a11deb804830b505
Dimas Rizki Mahendra <dimas1.mahendra@gmail.com>
2023-08-24 21:52:09 +0700

    Update Web Routes
    classify routes based on the role permissions they have

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use App\Livewire\RolesPermissions\EditApplyPermissions;
 use App\Livewire\RolesPermissions\EditApplyRoles;
 use App\Livewire\RolesPermissions\ShowApplyPermission;
 use App\Livewire\RolesPermissions\ShowApplyRoles;
+use App\Livewire\Users\CreateUsers;
+use App\Livewire\Users\EditUsers;
+use App\Livewire\Users\ShowUsers;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
@@ -28,25 +31,32 @@ Route::middleware(['auth'])->group(function () {
     Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
     Route::get('profile/{user}', ProfileInformation::class)->name('profile');
 
-    Route::prefix('permissions')->group(function () {
-        Route::get('', ShowPermissions::class)->name('permissions.index');
-        Route::get('create', CreatePermissions::class)->name('permissions.create');
-        Route::get('{permission:name}/edit', EditPermissions::class)->name('permissions.edit');
+    Route::group(['middleware' => ['role:pemilik']], function () {
+        Route::prefix('permissions')->group(function () {
+            Route::get('', ShowPermissions::class)->name('permissions.index');
+            Route::get('create', CreatePermissions::class)->name('permissions.create');
+            Route::get('{permission:name}/edit', EditPermissions::class)->name('permissions.edit');
+        });
+
+        Route::prefix('roles')->group(function () {
+            Route::get('', ShowRoles::class)->name('roles.index');
+            Route::get('create', CreateRoles::class)->name('roles.create');
+            Route::get('{role:name}/edit', EditRoles::class)->name('roles.edit');
+        });
+
+        Route::prefix('role-and-permission')->group(function () {
+            Route::get('permission/assignable', ShowApplyPermission::class)->name('assignable.index');
+            Route::get('permission/create', ApplyPermissions::class)->name('assignable.create');
+            Route::get('permission/{role:name}/edit', EditApplyPermissions::class)->name('assignable.edit');
+
+            Route::get('role/assign', ShowApplyRoles::class)->name('assign.index');
+            Route::get('role/create', ApplyRoles::class)->name('assign.create');
+            Route::get('role/{user}/edit', EditApplyRoles::class)->name('assign.edit');
+        });
+
+        Route::get('users', ShowUsers::class)->name('users.index');
+        Route::get('users/create', CreateUsers::class)->name('users.create');
+        Route::get('users/{user}/edit', EditUsers::class)->name('users.edit');
     });
 
-    Route::prefix('roles')->group(function () {
-        Route::get('', ShowRoles::class)->name('roles.index');
-        Route::get('create', CreateRoles::class)->name('roles.create');
-        Route::get('{role:name}/edit', EditRoles::class)->name('roles.edit');
-    });
-
-    Route::prefix('role-and-permission')->group(function () {
-        Route::get('permission/assignable', ShowApplyPermission::class)->name('assignable.index');
-        Route::get('permission/create', ApplyPermissions::class)->name('assignable.create');
-        Route::get('permission/{role:name}/edit', EditApplyPermissions::class)->name('assignable.edit');
-
-        Route::get('role/assign', ShowApplyRoles::class)->name('assign.index');
-        Route::get('role/create', ApplyRoles::class)->name('assign.create');
-        Route::get('role/{user}/edit', EditApplyRoles::class)->name('assign.edit');
-    });
 });
