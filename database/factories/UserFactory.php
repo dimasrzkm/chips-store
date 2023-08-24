bf8b5e4851ef6ab8b93c6e05889ec0043f49a9e2
Dimas Rizki Mahendra <dimas1.mahendra@gmail.com>
2023-08-24 21:59:36 +0700

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             // disable for login test case, enable again if needed
-            // 'email' => fake()->unique()->safeEmail(),
-            'email' => 'dimas@gmail.com',
+            'email' => fake()->unique()->safeEmail(),
+            // 'email' => 'dimas@gmail.com',
             'username' => fake()->userName(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
