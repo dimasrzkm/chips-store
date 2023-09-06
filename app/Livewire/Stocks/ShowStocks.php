826b80972b330c168e653d402291cbfa092acd6f
Dimas Rizki Mahendra <dimas1.mahendra@gmail.com>
2023-09-06 15:03:42 +0700

    Resources for Show Stocks

---
+++ app/Livewire/Stocks/ShowStocks.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Livewire\Stocks;
+
+use App\Livewire\Forms\StocksForm;
+use App\Models\Stock;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowStocks extends Component
+{
+    use WithPagination;
+
+    public StocksForm $form;
+
+    public function setHowMuchPageShow($total)
+    {
+        $this->form->showPerPage = $total;
+    }
+
+    public function getDataForDelete(Stock $stock)
+    {
+        $this->form->setPost($stock);
+    }
+
+    public function deleteStock()
+    {
+        $this->form->destroy();
+
+        return $this->redirectRoute('stocks.index', navigate: true);
+    }
+
+    #[Title('Data Bahan Baku')]
+    public function render()
+    {
+        $stocks = Stock::with('supplier')->orderby('tanggal_pengadaan', 'desc')->paginate($this->form->showPerPage, pageName: 'stock-page');
+        if ($this->form->search !== '') {
+            $stocks = Stock::with('supplier')
+                ->whereHas('supplier', function (Builder $query) {
+                    return $query->where('name', 'like', '%'.$this->form->search.'%');
+                })
+                ->orWhere('nama', 'like', '%'.$this->form->search.'%')
+                ->orWhere('tanggal_pengadaan', 'like', '%'.$this->form->search.'%')
+                ->paginate($this->form->showPerPage, pageName: 'stock-page');
+        }
+
+        return view('livewire.stocks.show-stocks', [
+            'stocks' => $stocks,
+        ]);
+    }
+}
