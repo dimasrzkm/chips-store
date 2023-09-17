c19f84bbd003ca607f8cf0be6f90c1df901b677d
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-09-17 11:51:12 +0700

    Pint refactor

--- app/Http/Middleware/UpgradeToHttpsUnderNgrok.php
+++ app/Http/Middleware/UpgradeToHttpsUnderNgrok.php
@@ -16,9 +16,10 @@ class UpgradeToHttpsUnderNgrok
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(str_ends_with($request->getHost(), '.ngrok-free.app')){
+        if (str_ends_with($request->getHost(), '.ngrok-free.app')) {
             URL::forceScheme('https');
         }
+
         return $next($request);
     }
 }
