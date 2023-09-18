aa2110749ccb785cfa75176e46005b2dce4209aa
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-09-18 08:46:58 +0700

    Resources for Product Migration

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'initial_price',
+        'percentage_profit',
+        'sale_price',
+        'stock',
+    ];
+}
---
+++ database/migrations/2023_09_17_131754_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('initial_price', 8, 2);
+            $table->float('percentage_profit', 8, 2);
+            $table->float('sale_price', 8, 2);
+            $table->float('stock', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
