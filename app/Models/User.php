bbd6339b06cd328afc67977614409091fddc8308
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-09-29 22:24:14 +0700

    Update Model User
    add relation to sellings

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Consigment::class);
     }
+
+    public function sellings()
+    {
+        return $this->hasMany(Selling::class);
+    }
 }
