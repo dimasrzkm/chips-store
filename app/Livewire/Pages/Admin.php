1129fd2fc6b28b0855e6f9a58b394046c316c637
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-10-05 11:54:25 +0700

    dashboard page

--- app/Livewire/Pages/Admin.php
+++ app/Livewire/Pages/Admin.php
@@ -2,14 +2,67 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Product;
+use App\Models\Selling;
+use Asantibanez\LivewireCharts\Models\ColumnChartModel;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Admin extends Component
 {
+    public $products;
+
+    public $nameOfProduct;
+
+    public $colorsOfProduct;
+
+    public $firstRun = true;
+
+    public $totalProducts;
+
+    public $totalSellings;
+
+    public function mount()
+    {
+        $this->nameOfProduct = Product::all()->pluck('name');
+        $this->products = Product::with('sellings')->whereIn('name', $this->nameOfProduct)->get();
+        $this->totalProducts = $this->nameOfProduct->count();
+        $this->totalSellings = Selling::count();
+        $colors = [
+            '#f6ad55', '#fc8181', '#f97316', '#86efac', '#059669', '#5eead4', '#06b6d4', '#075985',
+            '#ec4899', '#172554', '#15803d', '#94a3b8',
+        ];
+        foreach ($this->nameOfProduct as $index => $product) {
+            $this->colorsOfProduct[$product] = $colors[$index];
+        }
+    }
+
     #[Title('Dashboard')]
     public function render()
     {
-        return view('livewire.pages.admin');
+        $columnChartModel = $this->products->groupBy('name')
+            ->reduce(function (ColumnChartModel $columnChartModel, $data) {
+                // dd($data->first()->sellings->reduce(fn ($prev, $nex) => $prev += $nex->pivot->quantity));
+                $name = $data->first()->name;
+                // dd($name);
+                $value = $data->first()->sellings->reduce(fn ($prev, $nex) => $prev += $nex->pivot->quantity);
+
+                // dd($value);
+                // dd($this->colorsOfProduct);
+                return $columnChartModel->addColumn($name, $value, $this->colorsOfProduct[$name]);
+            }, (new ColumnChartModel())
+                ->setTitle('Produk Terjual')
+                ->setAnimated($this->firstRun)
+                ->withOnColumnClickEventName('onColumnClick')
+            );
+
+        return view('livewire.pages.admin', compact('columnChartModel'));
+    }
+
+    public function handleOnColumnClick($event)
+    {
+        $this->products = Product::with('sellings')->whereIn('name', $this->nameOfProduct)->get();
+        $this->totalProducts = $this->products->count();
+        $this->totalSellings = Selling::count();
     }
 }
--- resources/views/livewire/pages/admin.blade.php
+++ resources/views/livewire/pages/admin.blade.php
@@ -1,3 +1,23 @@
-<div class="flex">
-    <h2>Halo Selamat Datang</h2>
+<div wire:poll.30s="handleOnColumnClick(1)">
+    <div class="grid gap-5 md:grid-cols-2">
+        <div class="shadow stats">
+            <div class="stat">
+                <div class="stat-title">Total Produk</div>
+                <div class="stat-value">{{ $totalProducts }}</div>
+                <div class="stat-desc">Produk yang tersedia</div>
+            </div>
+        </div>
+        <div class="shadow stats">
+            <div class="stat">
+                <div class="stat-title">Total Transaksi</div>
+                <div class="stat-value">{{ $totalSellings }}</div>
+                <div class="stat-desc">Transaksi telah dilakukan</div>
+            </div>
+        </div>
+    </div>
+    <div class="h-80">
+        <livewire:livewire-column-chart key="{{ $columnChartModel->reactiveKey() }}" :column-chart-model="$columnChartModel" />
+    </div>
+    {{-- <livewire:scripts /> --}}
+    @livewireChartsScripts
 </div>
