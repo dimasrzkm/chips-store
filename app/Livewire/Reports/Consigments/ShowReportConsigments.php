957456bab48813ddbb21cc7fa43445a8b4047f35
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-10-09 00:34:11 +0700

    update show reports consigments
    check if consigment is not null

--- app/Livewire/Reports/Consigments/ShowReportConsigments.php
+++ app/Livewire/Reports/Consigments/ShowReportConsigments.php
@@ -31,24 +31,30 @@ class ShowReportConsigments extends Component
     {
         $this->validate();
         $consigments = Consigment::with('products')->find($this->consigmentId);
-        $pdf = Pdf::loadView('pdf.payments', [
-            'consigmentDate' => $consigments->consigment_date, // mengambil tanggal penitipan produk
-            'products' => $consigmentsProduct = $consigments->products, // mengambil data produk yang dititipkan
-            'konsinyorName' => $consigmentsProduct[0]->pivot->konsinyor_name, // nama penitip
-            'nameMakingReport' => auth()->user()->name, // nama pembuat laporan
-        ])->setPaper('a4', 'landscape')->output();
-        if (! $consigments->is_paid_off) {
-            PaidOffConsigment::dispatch($consigments); // menjalankan event untuk pengurangan stok
+        if (! is_null($consigments)) {
+            $pdf = Pdf::loadView('pdf.payments', [
+                'consigmentDate' => $consigments->consigment_date, // mengambil tanggal penitipan produk
+                'products' => $consigmentsProduct = $consigments->products, // mengambil data produk yang dititipkan
+                'konsinyorName' => $consigmentsProduct[0]->pivot->konsinyor_name, // nama penitip
+                'nameMakingReport' => auth()->user()->name, // nama pembuat laporan
+            ])->setPaper('a4', 'landscape')->output();
+            if (! $consigments->is_paid_off) {
+                PaidOffConsigment::dispatch($consigments); // menjalankan event untuk pengurangan stok
+            } else {
+                $this->reset('selectedConsigment');
+    
+                return back()->with('status', 'Produk titipan dengan nomor transaksi '.$consigments->transaction_code.' telah dilunaskan');
+            }
+    
+            return response()->streamDownload(
+                fn () => print($pdf),
+                'bukti_pelunasan.pdf'
+            );
         } else {
-            $this->reset('selectedConsigment');
-
-            return back()->with('status', 'Produk titipan dengan nomor transaksi '.$consigments->transaction_code.' telah dilunaskan');
+            $this->reset('consigmentId');
+            return back()->with('status', 'Pilih No Transaksi Penitipan!');
         }
-
-        return response()->streamDownload(
-            fn () => print($pdf),
-            'bukti_pelunasan.pdf'
-        );
+        
     }
 
     public function rules()
