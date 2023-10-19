c816dcaea15cbe9bce2bc89d9cd76c55e64615d8
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-10-19 08:33:04 +0700

    update reports
    change dropdown to only button

--- resources/views/livewire/reports/sellings/show-report-sellings.blade.php
+++ resources/views/livewire/reports/sellings/show-report-sellings.blade.php
@@ -33,8 +33,22 @@
                         @enderror
                     </label>
                 </div>
+                <div class="form-control">
+                    @if ($tanggal_awal != '' && $tanggal_akhir != '')
+                        <a type="button"
+                            class="block px-3 py-2 font-semibold leading-relaxed text-center border border-gray-300 rounded hover:bg-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 hover:cursor-pointer"
+                            target="_blank"
+                            href="{{ route('laporan.penjualan', [
+                                'tanggalAwal' => Crypt::encryptString($tanggal_awal),
+                                'tanggalAkhir' => Crypt::encryptString($tanggal_akhir),
+                            ]) }}">Cetak</a>
+                    @else
+                        <a wire:click="exportAs('pdf')" type="button"
+                            class="block px-3 py-2 font-semibold leading-relaxed text-center border border-gray-300 rounded hover:bg-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 hover:cursor-pointer">Cetak</a>
+                    @endif
+                </div>
                 {{-- menambah w-full untuk satu component agar fit dengan card --}}
-                <div class="w-full dropdown sm:dropdown-end">
+                {{-- <div class="w-full dropdown sm:dropdown-end">
                     <label tabindex="0"
                         class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 hover:cursor-pointer">
                         <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
@@ -56,7 +70,7 @@
                             <li><a wire:click="exportAs('pdf')">Pdf</a></li>
                         @endif
                     </ul>
-                </div>
+                </div> --}}
             </div>
 
         </div>
