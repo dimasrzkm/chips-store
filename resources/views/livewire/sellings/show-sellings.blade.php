ecf271fa7555364d11234a2ca823b1f77baa7b0c
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-11-20 21:01:05 +0700

    Update Show Selling
    Delete Button "Hapus"

--- resources/views/livewire/sellings/show-sellings.blade.php
+++ resources/views/livewire/sellings/show-sellings.blade.php
@@ -69,9 +69,9 @@
                             <th scope="col" class="px-4 py-3">Jumlah</th>
                             <th scope="col" class="px-4 py-3">Satuan Pembelian</th>
                             <th scope="col" class="px-4 py-3">Struk Pembayaran</th>
-                            <th scope="col" class="px-4 py-3">
+                            {{-- <th scope="col" class="px-4 py-3">
                                 <span class="sr-only">Actions</span>
-                            </th>
+                            </th> --}}
                         </tr>
                     </thead>
                     <tbody>
@@ -141,6 +141,7 @@
                                                             d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
                                                     </svg>
                                                 </a>
+                                                {{-- Tombol Generate QR Code --}}
                                                 {{-- <button type="button" wire:click="printReceipt({{ $selling }})"
                                                     class="p-2 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -152,31 +153,32 @@
                                                 </button> --}}
                                             @endif
                                         </td>
-                                        <td rowspan="{{ $selling->products->count() }}" class="px-4 py-3 ">
-                                            {{-- @can('menghapus catatan pengeluaran') --}}
-                                            <div class="dropdown dropdown-left dropdown-end">
-                                                <button tabindex="0"
-                                                    class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                                                    type="button">
-                                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
-                                                        viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                    </svg>
-                                                </button>
-                                                <ul tabindex="0"
-                                                    class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                                    <li>
-                                                        <!-- Open the modal using ID.showModal() method -->
-                                                        <a onclick="my_modal_1.showModal()"
-                                                            wire:click="getDataForDelete({{ $selling }})"
-                                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Hapus
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            {{-- @endcan --}}
-                                        </td>
+                                        {{-- Tombol Hapus Transaksi --}}
+                                        {{-- <td rowspan="{{ $selling->products->count() }}" class="px-4 py-3 ">
+                                            @can('menghapus catatan pengeluaran')
+                                                <div class="dropdown dropdown-left dropdown-end">
+                                                    <button tabindex="0"
+                                                        class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                                                        type="button">
+                                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                            viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                                        </svg>
+                                                    </button>
+                                                    <ul tabindex="0"
+                                                        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                                        <li>
+                                                            <!-- Open the modal using ID.showModal() method -->
+                                                            <a onclick="my_modal_1.showModal()"
+                                                                wire:click="getDataForDelete({{ $selling }})"
+                                                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Hapus
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            @endcan
+                                        </td> --}}
                                     @endif
                                 </tr>
                             @endforeach
